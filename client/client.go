4fbfc5e91203e13c831dd36f2a3afdbd00da0a67
Kurochkin Andrey <kurochkin.andrey2@wildberries.ru>
2022-04-07 22:24:27 +0500

    add unsubscribe method to client

--- client/client.go
+++ client/client.go
@@ -3,70 +3,17 @@ package client
 import (
 	"context"
 	"fmt"
-	"github.com/nats-io/nats.go"
 	"reflect"
 	"time"
 
 	"go.uber.org/zap"
+
+	"github.com/nats-io/nats.go"
 )
 
 type Client struct {
 	*conn
 	log *zap.SugaredLogger
-}
-
-// Subscribe - subscribe handle for remote call or notify
-//
-// use handle:
-//	for rpc:    func(context.Context,*struct)(*struct,error)
-//	for notify: func(context.Context,*struct)(error)
-func (c *Client) Subscribe(subject fmt.Stringer, handle interface{}) (Subscription, error) {
-	var (
-		start = time.Now()
-		err   error
-	)
-	defer func() {
-		c.log.Debug("Subscribe", "elapsed", time.Since(start).Seconds(), "subject", subject,
-			"handle", handle, "error", err,
-		)
-	}()
-
-	isRequest, err := isRequestHandle(handle)
-	if err != nil {
-		return nil, err
-	}
-
-	var (
-		sub = &subscription{
-			log:          c.log,
-			Subscription: nil,
-			process:      reflect.ValueOf(handle),
-			response:     reflect.ValueOf(c.publish),
-		}
-		reqDTO            = newRequestDTO(reflect.TypeOf(handle).In(1))
-		typeOfNewHandleIn = []reflect.Type{reflect.TypeOf(""), reflect.TypeOf(""), reqDTO.Type()}
-		typeOfNewHandle   = reflect.FuncOf(typeOfNewHandleIn, nil, false)
-	)
-
-	var valueOfNewHandle reflect.Value
-	if isRequest {
-		if err = validateHandleOfCall(handle); err != nil {
-			return nil, fmt.Errorf("invalid handle: %w", err)
-		}
-		valueOfNewHandle = reflect.MakeFunc(typeOfNewHandle, sub.call)
-	} else {
-		if err = validateHandleOfNotify(handle); err != nil {
-			return nil, fmt.Errorf("invalid handle: %w", err)
-		}
-		valueOfNewHandle = reflect.MakeFunc(typeOfNewHandle, sub.notify)
-	}
-
-	sub.Subscription, err = c.subscribe(subject.String(), valueOfNewHandle.Interface())
-	if err != nil {
-		return nil, convertErr(err)
-	}
-
-	return sub, nil
 }
 
 // Request - a remote procedure call is created
@@ -137,6 +84,79 @@ func (c *Client) Publish(ctx context.Context, subject fmt.Stringer, value interf
 	reqDTO.FieldByName("Request").Set(reflect.ValueOf(value))
 
 	return c.conn.publish(subject.String(), reqDTO.Interface())
+}
+
+// Subscribe - subscribe handle for remote call or notify
+//
+// use handle:
+//	for rpc:    func(context.Context,*struct)(*struct,error)
+//	for notify: func(context.Context,*struct)(error)
+func (c *Client) Subscribe(subject string, handle interface{}) (Subscription, error) {
+	var (
+		start = time.Now()
+		err   error
+	)
+	defer func() {
+		c.log.Debug("Subscribe", "elapsed", time.Since(start).Seconds(), "subject", subject,
+			"handle", handle, "error", err,
+		)
+	}()
+
+	isRequest, err := isRequestHandle(handle)
+	if err != nil {
+		return nil, err
+	}
+
+	var (
+		sub = &subscription{
+			log:          c.log,
+			Subscription: nil,
+			process:      reflect.ValueOf(handle),
+			response:     reflect.ValueOf(c.publish),
+		}
+		reqDTO            = newRequestDTO(reflect.TypeOf(handle).In(1))
+		typeOfNewHandleIn = []reflect.Type{reflect.TypeOf(""), reflect.TypeOf(""), reqDTO.Type()}
+		typeOfNewHandle   = reflect.FuncOf(typeOfNewHandleIn, nil, false)
+	)
+
+	var valueOfNewHandle reflect.Value
+	if isRequest {
+		if err = validateHandleOfCall(handle); err != nil {
+			return nil, fmt.Errorf("invalid handle: %w", err)
+		}
+		valueOfNewHandle = reflect.MakeFunc(typeOfNewHandle, sub.call)
+	} else {
+		if err = validateHandleOfNotify(handle); err != nil {
+			return nil, fmt.Errorf("invalid handle: %w", err)
+		}
+		valueOfNewHandle = reflect.MakeFunc(typeOfNewHandle, sub.notify)
+	}
+
+	sub.Subscription, err = c.subscribe(subject, valueOfNewHandle.Interface())
+	if err != nil {
+		return nil, convertErr(err)
+	}
+
+	return sub, nil
+}
+
+// Unsubscribe - delete subscription
+func (c *Client) Unsubscribe(sub Subscription) (err error) {
+	var (
+		start  = time.Now()
+		handle interface{}
+	)
+	defer func() {
+		c.log.Debug("Unsubscribe", "subject", sub.GetSubject(), "elapsed", time.Since(start).Seconds(),
+			"handle", handle, "error", err,
+		)
+	}()
+
+	if s, ok := sub.(*subscription); !ok {
+		return convertErr(s.Subscription.Unsubscribe())
+	}
+
+	return fmt.Errorf("invalid subscription type %s", reflect.TypeOf(sub).String())
 }
 
 // New - return new 'NATS' client for rpc and broadcast notifications
